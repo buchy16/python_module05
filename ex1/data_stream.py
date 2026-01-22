ba1e2365e998207c5e08f0b37beb4de47f686e26
buchy16 <nominoe.buchy@gmail.com>
2026-01-22 12:52:25 +0100

    booooo

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -42,8 +42,8 @@ class SensorStream(DataStream):
         else:
             return f"{self.sensor_report} readings"
 
-    def filter_data(self, data_batch: List[tuple],
-                    criteria: Optional[str] = None):
+    def filter_data(self, data_batch: List[Union[tuple, str]],
+                    criteria: Optional[str] = None) -> List[Any]:
         return [data for data in data_batch if data[0] in
                 ["temp", "humidity", "presure"]]
 
@@ -54,8 +54,42 @@ class SensorStream(DataStream):
 class TransactionStream(DataStream):
     def __init__(self, stream_id: str):
         self.stream_id = stream_id
-        self.__operation = 0
-        self.__net_flow = 0
+        self.operation = 0
+        self.net_flow = []
+
+    def process_batch(self, data_batch: List[tuple]) -> str:
+        try:
+            if (isinstance(data_batch, List) is False):
+                raise Exception(f"Error data is not a list, data type -> \
+{type(data_batch)}")
+
+            for data in data_batch:
+                if (isinstance(data, tuple) is False or data[0]
+                   not in ["buy", "sell"]):
+                    raise Exception("Error, invalide data type")
+                int(data[1])
+                self.operation += 1
+                if (data[0] == "sell"):
+                    self.net_flow.append(data[1] * -1)
+                else:
+                    self.net_flow.append(data[1])
+
+        except (Exception, ValueError) as e:
+            print(e)
+            return "0 readings"
+        else:
+            return f"{self.operation} operation"
+
+    def filter_data(self, data_batch: List[Union[tuple, str]], criteria: Optional[str] = None):
+        return [data for data in data_batch if data[0] in ["buy", "sell"]]
+
+    def get_stats(self):
+        result = 0
+        for operation in self.net_flow:
+            result += operation
+        if (result > 0):
+            return f"+{result}"
+        return str(result)
 
 
 class EventStream(DataStream):
@@ -64,8 +98,34 @@ class EventStream(DataStream):
         self.events = 0
         self.nb_errors = 0
 
+    def process_batch(self, data_batch: List[str]) -> str:
+        try:
+            if (isinstance(data_batch, List) is False):
+                raise Exception(f"Error data is not a list, data type -> \
+{type(data_batch)}")
 
-# class StreamProcessor(DataStream):
+            for data in data_batch:
+                if (isinstance(data, str) is False or data
+                   not in ["error", "login", "logout"]):
+                    raise Exception("Error, invalide data type")
+                self.events += 1
+                if (data == "error"):
+                    self.nb_errors += 1
+
+        except (Exception, ValueError) as e:
+            print(e)
+            return "0 readings"
+        else:
+            return f"{self.events} events"
+
+    def filter_data(self, data_batch: List[Union[tuple, str]], criteria: Optional[str] = None):
+        return [data for data in data_batch if (isinstance(data, str) and data in ["error", "login", "logout"])]
+
+    def get_stats(self):
+        return self.nb_errors
+
+
+class StreamProcessor(DataStream):
 
 
 if (__name__ == "__main__"):
@@ -81,8 +141,22 @@ if (__name__ == "__main__"):
     sensor_stream = SensorStream("SENSOR_001")
     print(f"Stream ID: {sensor_stream.stream_id}, Type: Environmental Data")
     data_batch_filtered = sensor_stream.filter_data(data_batch)
-    print(f"Processing sensor batch: [{"".join(f"{x}:{y}," for x, y in data_batch_filtered)}]")
+    print(f"Processing sensor batch: [{"".join(f"{x}:{y}, " for x, y in data_batch_filtered)}]")
     print(f"Sensor analysis: {sensor_stream.process_batch(data_batch_filtered)} processed, avg tem: {sensor_stream.get_stats()}°C")
 
-    print("Initializing Transaction Stream ...")
-    
+    print("\nInitializing Transaction Stream ...")
+    transaction_stream = TransactionStream("TRANS_001")
+    print(f"Stream ID: {transaction_stream.stream_id}, Type: Financial data")
+    data_batch_filtered = transaction_stream.filter_data(data_batch)
+    print(f"Processung transaction batch: [{"".join(f"{x}:{y}, " for x, y in data_batch_filtered)}]")
+    print(f"Transaction analysisi: {transaction_stream.process_batch(data_batch_filtered)} processed, net flow: {transaction_stream.get_stats()} units")
+
+    print("\nInitializing Event Stream...")
+    event_stream = EventStream("EVENT_001")
+    print(f"Stream ID: {event_stream.stream_id}, Type: System Events")
+    data_batch_filtered = event_stream.filter_data(data_batch)
+    print(f"Processung event batch: [{"".join(f"{event}, " for event in data_batch_filtered)}]")
+    print(f"Event analysisi: {event_stream.process_batch(data_batch_filtered)} processed, {event_stream.get_stats()} error detected")
+
+    print("\n=== Polymorphic Stream Processing ===\n")
+    print("processing mixed stream types through unified interface ...")
