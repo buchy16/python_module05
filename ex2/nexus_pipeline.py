ecc4b90055dcc380bfa7ac07f3d1af102b540ce0
buchy16 <nominoe.buchy@gmail.com>
2026-01-28 22:57:00 +0100

    Toutes les class Stages sont faites et fonctionnelles

--- ex2/nexus_pipeline.py
+++ ex2/nexus_pipeline.py
@@ -55,21 +55,90 @@ class ProcessingStage(Protocol):
 
 
 class InputSatge():
-    def process(self, data: Any) -> Dict:
-        pass
-    # vraie implémentation de la méthode
+    def process(self, data: Any) -> Any:
+        if (isinstance(data, dict)):
+            try:
+                for key, value in data.items():
+                    if (key not in ["sensor", "value", "unit"]):
+                        raise Exception("Error S1: Invalide data structure, valid structur is {sensor : ... , value : ... , unit : ...}")
+                    if ((isinstance(value, int) or isinstance(value, float)) is False and isinstance(value, str) is False):
+                        raise ValueError(f"Error S1: Invalide data found in data, {value} is not a string or an integer")
+                data["value"] = float(data["value"])
+            except (Exception, ValueError) as e:
+                print(e)
+                return None
+            else:
+                return data
+
+        elif (isinstance(data, str)):
+            try:
+                data_split = data.split(",")
+                if (len(data_split) < 3):
+                    raise Exception("Error S1: Invalide data structure, valid structur is 'user,action,timestamp'")
+            except (Exception) as e:
+                print(e)
+                return None
+            else:
+                return data
+
+        elif (isinstance(data, list)):
+            try:
+                for temp in data:
+                    float(temp)
+            except (ValueError) as e:
+                print(e)
+                return None
+            else:
+                return data
+        print("Error S1: Invalide data type")
 
 
 class TransformStage():
     def process(self, data: Any) -> Dict:
-        pass
-    # vraie implémentation de la méthode
+        if (isinstance(data, dict)):
+            try:
+                return ["Sensor_data", data]
+            except Exception:
+                print("Error S2: invalid data forma")
+                return None
+
+        if (isinstance(data, str)):
+            try:
+                return ["Log_data", {key: value for key, value in zip(["user", "action", "timestamp"], data.split(","))}]
+            except Exception:
+                print("Error S2: invalid data forma")
+                return None
+
+        if (isinstance(data, list)):
+            try:
+                return ["measure_data", {"temp_" + str(i): data[i] for i in range(0, len(data))}]
+            except Exception as e:
+                print(e)
+                return None
+        return data
 
 
 class OutputStage():
-    def process(data) -> str:
-        pass
-    # vraie implémentation de la méthode
+    def process(self, data: Any) -> str:
+        if (data is None):
+            return "An Error occured, output can't be generated"
+
+        if (data[0] == "Sensor_data"):
+            data = data[1]
+            if (data["sensor"] == "temp"):
+                return f"Processed temperatur reading: {data["value"]} {data["unit"]} (Normal Range)"
+            if (data["sensor"] == "hum"):
+                return f"Processed humidity reading: {data["value"]} {data["unit"]} (Normal Range)"
+
+        if (data[0] == "Log_data"):
+            data = data[1]
+            return f"User: {data["user"]}, activity: {data["action"]} at {data["timestamp"]}, 1 actions processed"
+
+        if (data[0] == "measure_data"):
+            data = data[1]
+            return f"Stream summary: {len(data)} readings, avg: {sum(data.values())}°C"
+
+
 
 
 # =============================================================================
@@ -99,5 +168,23 @@ if (__name__ == "__main__"):
     print("Pipeline capacity: 1000 stream/second")
 
     print("Creating Data Processing Pipeline...")
-    print(ProcessingPipeline.stages)
-    print(JSONAdapter.stages)
+    input = InputSatge()
+    transform = TransformStage()
+    output = OutputStage()
+    print("Stage 1: Input validation and parsing")
+    print("Stage 2: Data transformation and enrichment")
+    print("Stage 3: Output formatting and delivery\n")
+
+    print(" Multi-Format Data Processing ".center(30 + 6, "="))
+    print("\nProcessing JSON data through pipeline...")
+
+
+    print(output.process(transform.process(input.process("Shinji,login,8h30"))))
+    print(output.process(transform.process(input.process("Shinji,login"))))
+
+    print(output.process(transform.process(input.process({"sensor": "temp", "value": 23.5, "unit": "°C"}))))
+    print(output.process(transform.process(input.process({"car": "temp", "value": 23.5, "unit": "°C"}))))
+
+    print(output.process(transform.process(input.process([12, 78, 4.0, 15.0, -4, 2]))))
+    print(output.process(transform.process(input.process([12, 78, 4.0, "Emilie", -4, 2]))))
+
